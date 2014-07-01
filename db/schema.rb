c0534292cd1e814a36df4dc036a63b8cfa0c69a9
marvinick <marviluw@gmail.com>
2014-06-30 21:38:10 -0500

    user

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140628170923) do
+ActiveRecord::Schema.define(version: 20140701023632) do
 
   create_table "pages", force: true do |t|
     t.string   "title"
     t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
