7e9c605c668cd14b5422b83d7a30c57c503b225c
marvinick <marviluw@gmail.com>
2014-07-01 22:37:30 -0500

    email order MVC

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,6 +1,6 @@
 class AppMailer < ActionMailer::Base
   def order_confirmation(order)
     @order = order
-    mail_to: vendor.email, from: "chefmarvki@seasaltdev.com", subject: "Order for next delivery day"
+    mail to: order.email, from: "chefmarvki@seasaltdev.com", subject: "Order for next delivery day"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140702023941) do
+ActiveRecord::Schema.define(version: 20140702033600) do
 
   create_table "orders", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140702023941) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "vendor"
+    t.string   "email"
   end
 
   create_table "pages", force: true do |t|
