df34fd8299d0a840cef058262c5c27cb4be61c12
marvinick <marviluw@gmail.com>
2014-07-20 18:27:31 -0500

    catalog

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
   validates :title, :description, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
 end
