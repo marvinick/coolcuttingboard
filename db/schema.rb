3a3c147c61005545144b9d0047e6b8efd14810be
marvinick <marviluw@gmail.com>
2014-08-11 22:55:33 -0500

    creating user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140810133329) do
+ActiveRecord::Schema.define(version: 20140812033542) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20140810133329) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
